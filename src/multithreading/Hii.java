46325a307cf6395bd738c9bffd7ed12190833413
daudmujawar <daudmujawar3831@gmail.com>
2021-03-15 14:19:55 +0000

    Multithreading

---
+++ src/multithreading/Hii.java
@@ -0,0 +1,43 @@
+package multithreading;
+
+public class Hii extends Thread {
+    @Override
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hii");
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Hello extends Thread {
+    @Override
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello");
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class HiiHello {
+    public static void main(String[] args) {
+        Hii hii = new Hii();
+        Hello hello = new Hello();
+
+        hii.start();
+        hello.start();
+    }
+}
---
+++ src/multithreading/RunnableDemo.java
@@ -0,0 +1,15 @@
+package multithreading;
+
+public class RunnableDemo implements Runnable{
+    public static void main(String[] args) {
+        RunnableDemo runnableDemo=new RunnableDemo();
+        Thread thread=new Thread(runnableDemo);
+        thread.start();
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("In run method1");
+    }
+}
