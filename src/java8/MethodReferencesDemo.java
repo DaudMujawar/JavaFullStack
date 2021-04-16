a4834a5eaffc7600d2409fc25904ed6f028321ea
daudmujawar <daudmujawar3831@gmail.com>
2021-04-16 16:18:36 +0100

    Method Reference

--- src/java8/MethodReferencesDemo.java
+++ src/java8/MethodReferencesDemo.java
@@ -1,25 +1,36 @@
 package java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
+interface MyInterface1 {
+    String show();
+}
+
 public class MethodReferencesDemo {
 
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.forEach(System.out::println);
+
+
 //        MyInterface1 obj=MethodReferencesDemo::display; //for static methods
 //        System.out.println(obj.show());
 
-        MethodReferencesDemo obj=new MethodReferencesDemo();
-        MyInterface1 interface1=obj::display1;  //for non static methods
+        MethodReferencesDemo obj = new MethodReferencesDemo();
+        MyInterface1 interface1 = obj::display1;  //for non static methods
         System.out.println(obj.display1());
     }
 
-    private static String display(){
+    private static String display() {
         return "Hello";
     }
 
-    private String display1(){
+    private String display1() {
         return "Hiii";
     }
-}
-
-interface MyInterface1{
-    String show();
 }
