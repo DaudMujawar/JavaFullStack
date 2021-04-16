0271db3b4dd3f051bbc5a5abc524097c72c252c3
daudmujawar <daudmujawar3831@gmail.com>
2021-04-16 12:33:38 +0100

    String Builder

--- src/string/StringBuilderDemo.java
+++ src/string/StringBuilderDemo.java
@@ -4,7 +4,7 @@ public class StringBuilderDemo {
     public static void main(String[] args) {
         //        String buffer is immutable object
         //        String buffer is non synchronized
-        StringBuilder stringBuilder=new StringBuilder("JavaProgramming");
+        StringBuilder stringBuilder = new StringBuilder("JavaProgramming");
         stringBuilder.append("Language");
         System.out.println(stringBuilder);
     }
