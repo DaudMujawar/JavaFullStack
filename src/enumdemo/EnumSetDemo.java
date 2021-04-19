9644a3452c8cbdfea94073720ef660acac296a46
daudmujawar <daudmujawar3831@gmail.com>
2021-04-19 18:33:51 +0530

    Enum Set Demo

--- src/enumdemo/EnumSetDemo.java
+++ src/enumdemo/EnumSetDemo.java
@@ -1,11 +1,19 @@
 package enumdemo;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class EnumSetDemo {
-    enum Color{
-        WHITE,BLACK,ORANGE,BLUE
+    public static void main(String[] args) {
+        Set<Color> set = EnumSet.of(Color.WHITE, Color.BLUE);
+        Set<Color> colorSet = EnumSet.allOf(Color.class);
+
+        colorSet.forEach(s -> {
+            System.out.println(s);
+        });
     }
 
-    public static void main(String[] args) {
-
+    enum Color {
+        WHITE, BLACK, ORANGE, BLUE
     }
 }
