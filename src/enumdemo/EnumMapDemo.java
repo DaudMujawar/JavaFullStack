3e2276a79f4045923492e45f028c5c102d8ccf2c
daudmujawar <daudmujawar3831@gmail.com>
2021-04-19 18:29:21 +0530

    Enum Map Demp

--- src/enumdemo/EnumMapDemo.java
+++ src/enumdemo/EnumMapDemo.java
@@ -4,20 +4,20 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class EnumMapDemo {
-    enum Color{
-        WHITE,BLACK,ORANGE,BLUE
-    }
-
     public static void main(String[] args) {
-        EnumMap<Color,Integer> enumMap=new EnumMap<Color, Integer>(Color.class);
-        enumMap.put(Color.BLACK,1);
-        enumMap.put(Color.ORANGE,5);
-        enumMap.put(Color.BLUE,8);
+        EnumMap<Color, Integer> enumMap = new EnumMap<Color, Integer>(Color.class);
+        enumMap.put(Color.BLACK, 1);
+        enumMap.put(Color.ORANGE, 5);
+        enumMap.put(Color.BLUE, 8);
 
-        for (Map.Entry m:enumMap.entrySet()) {
-            System.out.println(m.getKey()+" "+m.getValue());
+        for (Map.Entry m : enumMap.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
 
         }
 
+    }
+
+    enum Color {
+        WHITE, BLACK, ORANGE, BLUE
     }
 }
---
+++ src/enumdemo/EnumSetDemo.java
@@ -0,0 +1,11 @@
+package enumdemo;
+
+public class EnumSetDemo {
+    enum Color{
+        WHITE,BLACK,ORANGE,BLUE
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
