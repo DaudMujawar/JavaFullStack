f861357f0cfb629a0d08fb5efdcd145bb807c97c
daudmujawar <daudmujawar3831@gmail.com>
2021-04-19 18:42:20 +0530

    Local date

--- src/java8/DataDemo.java
+++ src/java8/DataDemo.java
@@ -1,11 +1,24 @@
 package java8;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class DataDemo {
     public static void main(String[] args) {
-        Date date=new Date();
+        Date date = new Date();
         System.out.println(date);
-        System.out.println(System.currentTimeMillis());;
+        System.out.println(System.currentTimeMillis());
+
+        Date date1=new Date(1618837667058L);
+        System.out.println(date1);
+
+        LocalDate localDate=LocalDate.now();
+        LocalDate localDate1=LocalDate.now().minusDays(1);
+        LocalDate localDate2=LocalDate.now().plusDays(1);
+        System.out.println(localDate.getYear());
+        System.out.println(localDate.getDayOfMonth());
+
+        System.out.println(localDate1.isAfter(localDate));
+
     }
 }
