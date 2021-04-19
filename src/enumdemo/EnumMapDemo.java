b45a07b3291c865ad1974cde096310fd2d629948
daudmujawar <daudmujawar3831@gmail.com>
2021-04-19 18:27:13 +0530

    Enum Map Demo

---
+++ src/enumdemo/EnumMapDemo.java
@@ -0,0 +1,23 @@
+package enumdemo;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class EnumMapDemo {
+    enum Color{
+        WHITE,BLACK,ORANGE,BLUE
+    }
+
+    public static void main(String[] args) {
+        EnumMap<Color,Integer> enumMap=new EnumMap<Color, Integer>(Color.class);
+        enumMap.put(Color.BLACK,1);
+        enumMap.put(Color.ORANGE,5);
+        enumMap.put(Color.BLUE,8);
+
+        for (Map.Entry m:enumMap.entrySet()) {
+            System.out.println(m.getKey()+" "+m.getValue());
+
+        }
+
+    }
+}
