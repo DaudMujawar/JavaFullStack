c8ee87585717395a6540209e77e26dbbdae204d1
daudmujawar <daudmujawar3831@gmail.com>
2021-04-19 16:02:11 +0530

    Date

---
+++ src/java8/DataDemo.java
@@ -0,0 +1,11 @@
+package java8;
+
+import java.util.Date;
+
+public class DataDemo {
+    public static void main(String[] args) {
+        Date date=new Date();
+        System.out.println(date);
+        System.out.println(System.currentTimeMillis());;
+    }
+}
