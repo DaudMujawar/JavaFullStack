944d0ad53577a7113af08453deea5db2c41aff9b
daudmujawar <daudmujawar3831@gmail.com>
2021-04-19 18:30:59 +0530

    Enum Demo

--- src/enumdemo/EnumDemo.java
+++ src/enumdemo/EnumDemo.java
@@ -1,19 +1,20 @@
 package enumdemo;
 
 public class EnumDemo {
-    enum Color{
-        WHITE(1),ORANGE(2),BLACK(9),RED(6);
+    public static void main(String[] args) {
+        for (Color c : Color.values()) {
+            System.out.println(c + " " + c.vl);
 
-        int vl;
-        Color(int v){
-            this.vl=v;
         }
     }
 
-    public static void main(String[] args) {
-        for (Color c:Color.values()) {
-            System.out.println(c+" "+c.vl);
+    enum Color {
+        WHITE(1), ORANGE(2), BLACK(9), RED(6);
 
+        int vl;
+
+        Color(int v) {
+            this.vl = v;
         }
     }
 }
